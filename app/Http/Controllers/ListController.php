a137cab442898e2ad79e7d974f6a2748bdb2b60a
webdevmatics <webdevmatics@gmail.com>
2017-08-11 09:10:10 +0545

    lists crud

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Board;
+use App\Lists;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class BoardController extends Controller
+class ListController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -18,34 +19,47 @@ class BoardController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index($boardId)
     {
-        return response()->json(['boards'=> Auth::user()->boards],200);
+        $board=Board::find($boardId);
+
+          if (Auth::user()->id !== $board->user_id) {
+            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
+        }
+        return response()->json(['lists'=>$board->lists]);
     }
 
-    public function show($boardId)
+    public function show($boardId,$listId)
     {
-        $board = Board::findOrFail($boardId);
+        $board=Board::find($boardId);
+
+          if (Auth::user()->id !== $board->user_id) {
+            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
+        }
+
+        $list = $board->lists()->find($listId);
+
+
+        return response()->json(['status'=>'success','list'=>$list]);
+    }
+
+    public function store(Request $request, $boardId)
+    {
+        $this->validate($request,['name'=>'required']);
+
+        $board=Board::find($boardId);
 
         if (Auth::user()->id !== $board->user_id) {
             return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
         }
-
-        return response()->json(['board'=> $board],200);
-    }
-
-    public function store(Request $request)
-    {
-        $this->validate($request,['name'=>'required']);
-
-        Auth::user()->boards()->create([
+        $board->lists()->create([
             'name'    => $request->name,
         ]);
 
         return response()->json(['message' => 'success'], 200);
     }
 
-    public function update(Request $request, $boardId)
+    public function update(Request $request, $boardId,$listId)
     {
 
         $this->validate($request,['name'=>'required']);
@@ -61,15 +75,16 @@ class BoardController extends Controller
         return response()->json(['message' => 'success', 'board' => $board], 200);
     }
 
-    public function destroy($id)
+    public function destroy($boardId,$listId)
     {
-        $board=Board::find($id);
+        $board=Board::find($boardId);
 
         if(Auth::user()->id !== $board->user_id) {
             return response()->json(['status'=>'error','message'=>'unauthorized'],401);
         }
+        $list=$board->lists()->find($listId);
 
-        if (Board::destroy($id)) {
+        if ($list->delete()) {
             return response()->json(['status' => 'success', 'message' => 'Board Deleted Successfully']);
         }
 
